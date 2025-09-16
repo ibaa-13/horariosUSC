ac069a342863523d5603c7b8ed53ea600c5b96bd
ibaa-13 <ibaiencorreo@gmail.com>
2025-09-16 09:14:05 +0200

    Update loadconfig.py

--- loadconfig.py
+++ loadconfig.py
@@ -1,8 +1,9 @@
 import yaml
 
-CONFIG_PATH = r"C:\Users\usuario\python\horarios\config.yaml"
+CONFIG_PATH = r"python\horarios\config.yaml"
 
 def loadDefaultConfig():
     with open(CONFIG_PATH, "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
+
     return config
